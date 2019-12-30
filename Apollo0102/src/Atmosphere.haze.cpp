de93e29e931c538289657c87830b97860890363b
alexrhowe <53234440+alexrhowe@users.noreply.github.com>
2019-12-30 13:36:52 -0500

    Add files via upload

---
+++ Apollo0102/src/Atmosphere.haze.cpp
@@ -0,0 +1,347 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <math.h>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "constants.h"
+#include "Atmosphere.haze.h"
+
+using namespace std;
+using namespace cons;
+
+Atmosphere::Atmosphere(int hazeint, vector<double> hazeparams, string fname)
+{
+  hazetype = hazeint;
+  haze = hazeparams;
+  std::string hazeFile;
+  
+    switch (hazetype){
+      case 0: hazeFile = "../Opacities/blank.r.dat"; break;
+      case 1: hazeFile = "../Opacities/zns.indices.dat"; break;
+      case 2: hazeFile = "../Opacities/polyacet.indices.dat"; break;
+      case 3: hazeFile = "../Opacities/tholin.indices.r.dat";
+      case 4: hazeFile = fname;
+    }
+    // read in indices of refraction
+    std::ifstream hFilePrep(hazeFile.c_str());
+    if(!hFilePrep)
+      std::cout << "Haze Cross Section File Not Found" << std::endl;
+
+    string line;
+    hazeNumVals = 0;
+    while(getline(hFilePrep,line)){
+      hazeNumVals++;
+    }
+
+    elam = new double[hazeNumVals];
+    einn = new double[hazeNumVals];
+    eink = new double[hazeNumVals];
+    hFilePrep.close();
+
+    std::ifstream hFile(hazeFile.c_str());
+    for(int i=0; i<hazeNumVals; i++){
+      hFile >> elam[i] >> einn[i] >> eink[i];
+    }
+    ar = new double[20000];
+    ai = new double[20000];
+    br = new double[20000];
+    bi = new double[20000];
+  
+    rsr = new double[20000];
+    rsi = new double[20000];
+    rsx = new double[20000];
+    px = new double[20000];
+
+    asf=0.;
+    qe=0.;
+    qa=0.;
+    qs=0.;
+    qp=0.;
+    
+    s1=0.;
+    s2=0.;
+    pext=0.;
+    qext=0.;
+    hFile.close();
+}
+
+// General 2nd-order interpolation method used to interpolate complex indicies
+// ***Need to fix this***
+double Atmosphere::value(double x, double* sigma, double* e)
+{
+  double value;
+  int n = hazeNumVals;
+  if(x<e[0])
+    {
+      value = sigma[0] + (log(x)-log(e[0])) * (sigma[1]-sigma[0])/(log(e[1])-log(e[0]));
+      return value;
+    }
+  else if(x>e[n-2])
+    {
+      value = sigma[n-1] + (log(x)-log(e[n-1])) * (sigma[n-1]-sigma[n-2])/(log(e[n-1])-log(e[n-2]));
+      return value;
+    }
+  else
+    {
+      int ie=0;
+      for(int i=0; i<n; i++)
+	{
+	  if(x<e[i])
+	    {
+	      ie = i-1;
+	      break;
+	    }
+	}
+      double e0 = log(e[ie]);
+      double e1 = log(e[ie+1]);
+      double e2 = log(e[ie+2]);
+      double sig0 = log(sigma[ie]);
+      double sig1 = log(sigma[ie+1]);
+      double sig2 = log(sigma[ie+2]);
+      double ee = log(x);
+      
+      double a0 = (ee-e1)*(ee-e2)/((e0-e1)*(e0-e2));
+      double b0 = (ee-e0)*(ee-e2)/((e1-e0)*(e1-e2));
+      double c0 = (ee-e1)*(ee-e0)/((e2-e1)*(e2-e0));
+      double prob = a0*sig0 + b0*sig1 + c0*sig2;
+      value = exp(prob);
+      return value;
+    }
+}
+
+/*
+ * Calculates absorption and scattering efficiencies of spherical grains
+ * Maximum size parameter: 1.e5
+ * Inputs: size, wavelength, complex indices of refraction
+ * Outputs:
+ * qe = extinction efficiency = qa + qs
+ * qs = scattering efficiency
+ * qa = absorption efficiency
+ * qp = radiation pressure efficiency
+ * asf = asymmetrical factor
+ */
+void Atmosphere::MieLargeSzpara(double size, double wavelength, double xm, double ymm)
+{
+  int nx = 20000;
+  double drc = 0.017453292519943;
+  double rdc = 57.295779513082;
+  double thb = 0.0;
+  double thf = 180.0;
+  double thi = 10.0;
+  // xm = Re(refractive index)
+  // ym = -Im(refractive index)
+  double ym = -ymm;
+  double x = 2.*pi*size/wavelength;  // size parameter
+
+  double xn = sqrt(xm*xm + ym*ym)*x;
+  //int nx = (int)(1.10*xn + 10.0);           // dwn recursion nx for all sizes
+  double ro = 2.0*x*(xm-1.0);
+  int i = smi1xs(x,nx,xm,ym);
+  sm5msx(x,i);
+  asf = (qe-qp)/qs;
+  double xstops = x + 4.0*pow(x,1./3.) + 2.0; // cf Bohen-Huffman P480 (1983)
+  int nv = (int)(std::max(xstops,xn)+15.);             // limit smi3dm array
+  smi3sm(i,180.0);
+  double qb = 2.*(s1*s1 + s2*s2)/x/x;         // back-scatter efficiency
+  sm5pqs(x,i);
+  int nthd = (int)((thf-thb)/thi + 1.0);
+
+  double fi1=0.;
+  double fi2=0.;
+  for(int n=0; n<nthd; n++)
+    {
+      int thd = (int)(thb + (double)(n-1)*thi);
+      smi3sm(i,thd);
+      if(s1i==0.0) fi1 = pi/2.0;
+      if(s1i!=0.0)
+	{
+	  fi1 = atan(s1r/fabs(s1i));
+	  if(s1i<0.0) fi1 = pi-fi1;
+	  if(fi1<0.0) fi1 = 2.0*pi - fabs(fi1);
+	}
+      double fi1d = rdc*fi1;
+      if(s1i==0.0) fi2 = pi/2.0;
+      if(s2i!=0.0)
+	{
+	  fi2 = atan(s2r/fabs(s2i));
+	  if(s2i<0.0) fi2 = pi-fi2;
+	  if(fi2<0.0) fi2 = 2.0*pi - fabs(fi2);
+	}
+      double fi2d = rdc*fi2;
+      double s1s = s1*s1;
+      double s2s = s2*s2;
+      double sns = 0.50*(s1s+s2s);
+      double gn = 4.0*sns/x/x;
+      double pol = (s1s-s2s)/(s1s+s2s);
+    }
+  return;
+}
+
+// no explanation given
+int Atmosphere::smi1xs(double x, int nx, double xm, double ym)
+{
+  double scale;
+  double ctc = 1.e-14;              // edited from ctc=1.e-8
+  double xmx = xm*x;                // m'x
+  double ymx = ym*x;                // m"x
+  double rp2 = xmx*xmx + ymx*ymx;   // (m'x)^2 + (m"x)^2
+  double pnx = x/(2.*nx+3.);        // asymptotic value of pn(x) at n=nx+1
+  double pnr = xmx/(2.*nx+3.);      // value of Re(pn(z)) at n=nx+1
+  double pni = ymx/(2.*nx+3.);      // value of Im(pn(z)) at n=nx+1
+  // dwn recursion of pn(z)=s(n)/s(n-1)=(pnr,pni) and an(z)=s'(n)/s(n)=(rsr(n),rsi(n))
+  // also for real arguments: px(n) and rsx(n)
+  for(int kk=0; kk<nx; kk++)
+    {
+      int n = nx-kk-1;
+      double cn = (double)n+1.;
+      double aln = (2.*cn+1.)*xmx/rp2-pnr; // Re(1./pn(z))
+      double ben = (2.*cn+1.)*ymx/rp2+pni; // -Im(1./pn(z))
+      ar[n] = -cn*xmx/rp2 + aln;   // Re(an(z))
+      ai[n] = cn*ymx/rp2 - ben;    // Im(an(z))
+      double pzd = aln*aln + ben*ben;
+      pnr = aln/pzd;                // Re(pn(z))
+      pni = ben/pzd;                // Im(pn(z))
+      br[n] = (cn+1.)/x - pnx;      // s'(n)/s(n) for real x
+      if(n==0) break;               // avoid computing px(1) irregular at x=n*pai
+      pnx = x/(2.*cn+1.-pnx*x);
+      bi[n] = pnx;                  // p(n)=s(n)/s(n-1) for real x
+    }
+  // generation of Mie coefficient to such n as determined by ctc (the convergence test constant)
+  double snm1x = sin(x);
+  double cnm1x = cos(x);
+  double snx = 0.0;
+  // for Rayleigh sizes x<0.1, power series employed for snx = sin(x)/x - cos(x)
+  if(x<0.1) snx = x*x/3. - pow(x,4)/30. + pow(x,6)/840. - pow(x,8)/45360.;
+  if(x>0.1) snx = snm1x/x - cnm1x;
+  double cnx = cnm1x/x + snm1x;
+  int nf = 0;
+  for(int n=0; n<nx-1; n++)
+    {
+      double cn = (double)n+1.;
+      double dcnx = cnm1x - cn*cnx/x;
+      double dsnx = br[n]*snx;
+      double annr = ar[n]*snx - xm*dsnx;    // Re(numerator of a(n))
+      double anni = ai[n]*snx - ym*dsnx;    // Im(numerator of a(n))
+      double ta1 = ar[n]*snx - ai[n]*cnx;
+      double ta2 = ai[n]*snx + ar[n]*cnx;
+      double andr = ta1 - xm*dsnx + ym*dcnx; // Re(denominator of a(n))
+      double andi = ta2 - xm*dcnx - ym*dsnx; // Im(denominator of a(n))
+      double andc = andr*andr + andi*andi;
+      double bnnr = (xm*ar[n] - ym*ai[n]) * snx - dsnx; // Re(numerator of b(n))
+      double bnni = (xm*ai[n] + ym*ar[n]) * snx;        // Im(numerator of b(n))
+      double tb1 = ar[n]*snx - ai[n]*cnx;
+      double tb2 = ar[n]*cnx + ai[n]*snx;
+      double bndr = xm*tb1 - ym*tb2 - dsnx;               // Re(denominator of b(n))
+      double bndi = xm*tb2 + ym*tb1 - dcnx;               // Re(denominator of b(n))
+      double bndc = bndr*bndr + bndi*bndi;
+      ar[n] = (annr*andr + anni*andi)/andc; // Re(a(n))
+      ai[n] = (anni*andr - annr*andi)/andc; // Im(a(n))
+      br[n] = (bnnr*bndr + bnni*bndi)/bndc; // Re(b(n))
+      bi[n] = (bnni*bndr - bnnr*bndi)/bndc; // Im(b(n))
+      double ti = ar[n]*ar[n] + ai[n]*ai[n] + br[n]*br[n] + bi[n]*bi[n];
+      ti /= ar[0]*ar[0] + ai[0]*ai[0] + br[0]*br[0] + bi[0]*bi[0];
+      if(nx!=3)
+	{
+	  if(ti-ctc<=0.)
+	    {
+	      nf = n;
+	      break;
+	    }
+	}
+      if(n-nx>=0)
+	{
+	  nf = n;
+	  break;
+	}
+      snx *= bi[n+1];
+      double cnm2x = cnm1x;
+      cnm1x = cnx;
+      cnx = (2.*cn+1.)*cnm1x/x - cnm2x;
+      nf = n;
+    }
+  return nf;
+}
+
+// Sub sm5msx for Mie Q factors from smi1xs
+void Atmosphere::sm5msx(double x, int i)
+{
+  double s1r = 0.0;
+  double ssc = 0.0;
+  double spr = 0.0;
+  // i is number of iterations (I think)
+  for(int n=0; n<i-1; n++)
+    {
+      double cn = (double)n+1.;
+      double fs1r = 2.*cn+1.;
+      double fssc = fs1r;
+      double fspr1 = 2.*cn*(cn+2.)/(cn+1.);
+      double fspr2 = 2.*(2.*cn+1.)/(cn*(cn+1.));
+      s1r += fs1r * (ar[n]+br[n]);
+      ssc += fssc * (ar[n]*ar[n] + ai[n]*ai[n] + br[n]*br[n] + bi[n]*bi[n]);
+      spr += fspr1 * (ar[n]*ar[n+1] + ai[n]*ai[n+1] + br[n]*br[n+1] + bi[n]*bi[n+1]);
+      spr += fspr2 * (ar[n]*br[n] + ai[n]*bi[n]);
+    }
+
+  double fc = 2.0/x/x;
+  qe = fc*s1r;
+  qs = fc*ssc;
+  qa = qe-qs;
+  qp = qe-fc*spr;
+}
+
+// smi3sm for Mie angular functions
+void Atmosphere::smi3sm(int i, double thd)
+{
+  double scale;
+
+  double th = thd * 0.0174532925;
+  double z = cos(th);
+  double pn1 = 0.0;
+  double pn = 1.0;
+  double s1r = 0.0;
+  double s1i = 0.0;
+  double s2r = 0.0;
+  double s2i = 0.0;
+
+  // i seems to be the number of iterations
+  for(int n=0; n<i; n++)
+    {
+      double cn = (double)n+1.;
+      double tn = cn*z*pn - (cn-1.)*pn1;
+      double f = (2.*cn+1.)/(cn*cn+cn);
+      s1r += f * (ar[n]*pn + br[n]*tn);
+      s1i += f * (ai[n]*pn + bi[n]*tn);
+      s2r += f * (ar[n]*tn + br[n]*pn);
+      s2i += f * (ai[n]*tn + bi[n]*pn);
+      double pn2 = pn1;
+      pn1 = pn;
+      pn = (z*(2.*cn+1.)*pn1 - (cn+1.)*pn2)/cn;
+    }
+
+  double s1s = s1r*s1r + s1i*s1i;
+  double s2s = s2r*s2r + s2i*s2i;
+  s1 = sqrt(s1s);
+  s2 = sqrt(s2s);
+}
+
+// Sub sm5pqs for Mie P, Q comparison by smi1sx coefficients
+void Atmosphere::sm5pqs(double x, int nm)
+{
+  double scale;
+
+  double s1r = 0.0;
+  double s1i = 0.0;
+
+  for(int i=0; i<nm-1; i++)
+    {
+      double fs = 2.*i+1.;
+      s1r += fs*(ar[i]+br[i]);
+      s1i += fs*(ai[i]+bi[i]);
+    }
+
+  double fe = 2./x/x;
+  qext = fe*s1r;
+  pext = fe*s1i;
+}
---
+++ Apollo0102/src/Filter.py
@@ -0,0 +1,353 @@
+# Functions that return thruput for individual spectroscopic modes
+# for the JWST pipeline.
+
+import numpy as np
+from scipy import interpolate
+
+class Filter():
+    def __init__(self):
+        self.delwave = np.zeros(2048)
+        self.calwave = np.zeros(2048)
+        self.resolve = 1.0
+        self.slit = 1.0
+        self.omeg_back = 1.e-6
+        self.scale = 1.0
+        self.effic = 1.0
+        self.npix1 = 1
+        self.npix2 = 2
+        self.npix3 = 3
+        self.npix4 = 4
+        
+    def NIRSpec(self,fnum):
+        fnum = int(fnum)
+        fFilter = open('FilterList.dat','r')
+        flines = fFilter.readlines()
+        flength = len(flines)
+        filter = flines[fnum].split()[0]
+        fmin = float(flines[fnum].split()[1])
+        fmax = float(flines[fnum].split()[2])
+
+        if fnum<4:
+            self.delwave = (fmax-fmin)/1399.0
+            self.calwave = np.zeros(1400)
+        if 4<=fnum<8:
+            self.delwave = (fmax-fmin)/511.0
+            self.calwave = np.zeros(512)
+        if fnum==8:
+            self.delwave = (fmax-fmin)/255.0
+            self.calwave = np.zeros(256)
+        
+        for i in range(0,len(self.calwave)):
+            self.calwave[i] = fmin + self.delwave*i
+        
+        fin = open(filter,'r')
+        lines = fin.readlines()
+        length = len(lines)
+        microns = np.zeros(length)
+        thruput = np.zeros(length)
+        for i in range(0,length):
+            microns[i] = lines[i].split()[0]
+            thruput[i] = lines[i].split()[1]
+        
+        f = interpolate.interp1d(microns,thruput,kind='linear')
+        xthruput = f(self.calwave)
+
+        theta = 10.08  # half-angle of beam to the detector in degrees
+        self.scale = 0.065  # arcsec per pixel
+        self.slit = -999.   # IFU = slitless
+        self.resolve = 2.0  # 2 column resolution
+        
+        ngr = 4
+        self.effic = (ngr-1.)/(ngr+1.)
+        return xthruput
+    
+    def NIRSpecG395H(self):
+        self.delwave = (4.9-2.9)/2047.
+        self.calwave = np.zeros(2048)
+        for i in range(0,len(self.calwave)):
+            self.calwave[i] = 2.9 + self.delwave*i
+
+        # read in *.txt response function (photons to electrons) to xmicrons and xthruput       
+        fin = open('Filters/NIRSpec_G395H.dat','r')
+        lines = fin.readlines()
+        length = len(lines)
+        microns = np.zeros(length)
+        thruput = np.zeros(length)
+        for i in range(0,length):
+            microns[i] = lines[i].split()[0]
+            thruput[i] = lines[i].split()[1]
+
+        f = interpolate.interp1d(microns,thruput,kind='linear')
+        xthruput = f(self.calwave)
+
+        theta = 10.08       # half-angle of beam to the detector in degrees
+        self.scale = 0.065  # arcsec per pixel
+        self.slit = 24.6    # entrance slit size in columns, this is the 1.6x1.6 arcsec case
+        self.resolve = 2.0  # 2 column resolution
+        
+        ngr = 4
+        self.effic = (ngr-1.)/(ngr+1.)
+        return xthruput
+
+    def NIRCamF322W2(self):
+        self.effic = 0.8
+        npix = 1675
+        self.delwave = (5.0-2.4)/(npix-1.)
+        self.calwave = np.zeros(npix)
+        for i in range(0,len(self.calwave)):
+            self.calwave[i] = 2.4 + self.delwave*i
+
+        theta = 0.0
+        self.scale = 0.065
+        self.slit = -999.   # no slit
+        self.resolve = 2.0
+        tel = 0.94     # telescope efficiency
+        optics = 0.541 # instrument optics + filter
+        
+        gwave = np.zeros(29)  # grism wavelengths
+        for i in range(0,len(gwave)):
+            gwave[i] = 2.30 + 0.1*i
+            # grism thruput
+        grespon = [0.24, 0.24, 0.306,0.374,0.437,0.494,0.543,0.585,0.62, 0.648,0.67, 0.686,0.696,0.702,0.705,0.703,0.702,0.694,0.685,0.674,0.661,0.649,0.636,0.621,0.609,0.593,0.579,0.566,0.566]
+            # total instrument thruput
+        trespon2 = [0.10,0.10,0.13,0.16,0.19,0.22,0.24,0.26,0.29,0.30,0.32,0.34,0.36,0.37,0.37,0.37,0.36,0.35,0.35,0.34,0.32,0.30,0.28,0.26,0.24,0.23,0.20,0.10,0.10]
+            # detector QE
+        qe = 0.934871364 + 0.051540672*gwave - 0.281664062*gwave*gwave + 0.243866616*pow(gwave,3) - 0.086009299*pow(gwave,4) + 0.014508849*pow(gwave,5) - 0.001000108*pow(gwave,6)
+
+        qe = qe*0.88
+        trespon = np.zeros(27)
+        for i in range(0,27): trespon = tel*optics*grespon[i]*qe[i]
+
+        f = interpolate.interp1d(gwave,trespon2,kind='linear')
+        xthruput = f(self.calwave)
+            
+        return xthruput
+
+    def MIRILRS(self,slitin):
+        self.effic = 0.8
+        npix = 320   # number of detector pixels used by the spectrum, from a Kendrew figure
+        self.delwave = (14.0-5.0)/(npix-1.)
+        self.calwave = np.zeros(npix)
+        for i in range(0,len(self.calwave)):
+            self.calwave[i] = 5.0 + self.delwave*i
+
+        theta = 0.0
+        self.scale = 0.11
+        if(slitin==True):
+            self.slit = 4.6
+            self.resolve = 4.6
+            optics = 0.6
+        if(slitin==False):
+            self.slit = -999.0
+            self.resolve = 2.7
+            optics = 1.0
+
+        tel = 0.94
+        gwave = np.zeros(19)
+        grespon = [0.1049,0.1409,0.2292,0.2524,0.2945,0.2589,0.2631,0.3123,0.3047,0.2805,0.2602,0.2352,0.2156,0.1550,0.1132,0.0771,0.0502,0.0251,0.0251]
+        trespon = np.zeros(19)
+        for i in range(0,len(gwave)):
+            gwave[i] = 5.0 + 0.5*i
+            trespon[i] = tel*optics*grespon[i]
+
+        f = interpolate.interp1d(gwave,trespon,kind='linear')
+        xthruput = f(self.calwave)
+        return xthruput
+
+    def NIRISS(self):
+        # Alternative throughput function
+        #fin = open('Filters/NIRISS_Throughput.dat','r')
+        fin = open('Filters/NIRISS_G700XD.dat','r')
+        lines = fin.readlines()
+        nwave = np.zeros(len(lines))
+        nresponse = np.zeros(len(lines))
+        for i in range(0,len(lines)):
+            nwave[i] = lines[i].split()[0]
+            nresponse[i] = lines[i].split()[1]
+
+        self.delwave = (2.5-0.602)/2047.0
+        self.calwave = np.zeros(2048)
+        for i in range(0,len(self.calwave)):
+            self.calwave[i] = 0.602 + self.delwave*i
+
+        theta = 10.08
+        self.scale = 0.0658
+        self.slit = -999.0
+        self.resolv = 2.0
+        self.effic = 0.8
+
+        f = interpolate.interp1d(nwave,nresponse,kind='linear')
+        xthruput = f(self.calwave)
+        return xthruput
+
+    def MIRIMRS(self,channel):
+        self.effic = 0.8  # arbitrary for now
+        
+        if channel == 'A':
+            lam1 = 4.87
+            lam2 = 5.82
+            r1 = 3320.0
+            r2 = 3710.0
+        if channel == 'B':            
+            lam1 = 5.62
+            lam2 = 6.73
+            r1 = 3190.0
+            r2 = 3750.0
+        if channel == 'C':            
+            lam1 = 6.49
+            lam2 = 7.76
+            r1 = 3100.0
+            r2 = 3610.0
+
+        w1 = 1.405
+        w2 = 1.791
+        w = (w1+w2)/2.0   # slice width in pixels (width = dispersion direction)
+        r = (r1+r2)/2.0   # approximate the resolving power at mid-channel
+        self.npix1 = np.modf(w*(lam2-lam1)*r + 0.5)[1]
+        self.delwave = (lam2-lam1)/self.npix1
+        calwave1 = np.zeros(self.npix1)
+        for i in range(0,len(calwave1)):
+            calwave1[i] = lam1 + self.delwave*i
+        cwave11 = np.zeros(self.npix1)
+        cwave12 = np.zeros(self.npix1)  # wavelengths at the boundaries of the pixels
+
+        for i in range(0,self.npix1-2):
+            cwave12[i] = (calwave1[i] + calwave1[i+1])/2.0
+        for i in range(0,self.npix1-1):
+            cwave11[i] = (calwave1[i-1] + calwave1[i])/2.0
+        cwave11[0] = calwave1[0] - (cwave11[1] - calwave1[0])
+        cwave12[self.npix1-1] = calwave1[self.npix1-1] + (calwave1[self.npix1-1] - cwave12[self.npix1-2])
+        
+        if channel == 'A':
+            lam1 = 7.45
+            lam2 = 8.90
+            r1 = 2990.0
+            r2 = 3110.0
+        if channel == 'B':
+            lam1 = 8.61
+            lam2 = 10.28
+            r1 = 2750.0
+            r2 = 3170.0
+        if channel == 'C':
+            lam1 = 9.91
+            lam2 = 11.87
+            r1 = 2860.0
+            r2 = 3300.0
+
+        w1 = 1.452
+        w2 = 1.821
+        w = (w1+w2)/2.0   # slice width in pixels (width = dispersion direction)
+        r = (r1+r2)/2.0   # approximate the resolving power at mid-channel
+        self.npix2 = np.modf(w*(lam2-lam1)*r + 0.5)[1]
+        self.delwave = (lam2-lam1)/self.npix2
+        calwave2 = np.zeros(self.npix2)
+        for i in range(0,len(calwave2)):
+            calwave2[i] = lam1 + self.delwave*i
+        cwave21 = np.zeros(self.npix2)
+        cwave22 = np.zeros(self.npix2)  # wavelengths at the boundaries of the pixels
+
+        for i in range(0,self.npix2-2):
+            cwave22[i] = (calwave2[i] + calwave2[i+1])/2.0
+        for i in range(0,self.npix2-1):
+            cwave21[i] = (calwave2[i-1] + calwave2[i])/2.0
+        cwave21[0] = calwave2[0] - (cwave21[1] - calwave2[0])
+        cwave22[self.npix2-1] = calwave2[self.npix2-1] + (calwave2[self.npix2-1] - cwave22[self.npix2-2])
+        
+        if channel == 'A':
+            lam1 = 11.47
+            lam2 = 13.67
+            r1 = 2530.0
+            r2 = 2880.0
+        if channel == 'B':
+            lam1 = 13.25
+            lam2 = 15.80
+            r1 = 1790.0
+            r2 = 2640.0
+        if channel == 'C':
+            lam1 = 15.30
+            lam2 = 18.24
+            r1 = 1980.0
+            r2 = 2790.0
+
+        w1 = 1.629
+        w2 = 2.043
+        w = (w1+w2)/2.0   # slice width in pixels (width = dispersion direction)
+        r = (r1+r2)/2.0   # approximate the resolving power at mid-channel
+        self.npix3 = np.modf(w*(lam2-lam1)*r + 0.5)[1]
+        self.delwave = (lam2-lam1)/self.npix3
+        calwave3 = np.zeros(self.npix3)
+        for i in range(0,len(calwave3)):
+            calwave3[i] = lam1 + self.delwave*i
+        cwave31 = np.zeros(self.npix3)
+        cwave32 = np.zeros(self.npix3)  # wavelengths at the boundaries of the pixels
+
+        for i in range(0,self.npix3-2):
+            cwave32[i] = (calwave3[i] + calwave3[i+1])/2.0
+        for i in range(0,self.npix3-1):
+            cwave31[i] = (calwave3[i-1] + calwave3[i])/2.0
+        cwave31[0] = calwave3[0] - (cwave31[1] - calwave3[0])
+        cwave32[self.npix3-1] = calwave3[self.npix3-1] + (calwave3[self.npix3-1] - cwave32[self.npix3-2])
+        
+        if channel == 'A':
+            lam1 = 17.54
+            lam2 = 21.10
+            r1 = 1460.0
+            r2 = 1930.0
+        if channel == 'B':
+            lam1 = 20.44
+            lam2 = 24.72
+            r1 = 1680.0
+            r2 = 1770.0
+        if channel == 'C':
+            lam1 = 23.84
+            lam2 = 28.82
+            r1 = 1630.0
+            r2 = 1330.0
+
+        w1 = 2.253
+        w2 = 2.824
+        w = (w1+w2)/2.0   # slice width in pixels (width = dispersion direction)
+        r = (r1+r2)/2.0   # approximate the resolving power at mid-channel
+        self.npix4 = np.modf(w*(lam2-lam1)*r + 0.5)[1]
+        self.delwave = (lam2-lam1)/self.npix4
+        calwave4 = np.zeros(self.npix4)
+        for i in range(0,len(calwave4)):
+            calwave4[i] = lam1 + self.delwave*i
+        cwave41 = np.zeros(self.npix4)
+        cwave42 = np.zeros(self.npix4)  # wavelengths at the boundaries of the pixels
+
+        for i in range(0,self.npix4-2):
+            cwave42[i] = (calwave4[i] + calwave4[i+1])/2.0
+        for i in range(0,self.npix4-1):
+            cwave41[i] = (calwave4[i-1] + calwave4[i])/2.0
+        cwave41[0] = calwave4[0] - (cwave41[1] - calwave4[0])
+        cwave42[self.npix4-1] = calwave4[self.npix4-1] + (calwave4[self.npix4-1] - cwave42[self.npix4-2])
+
+        self.scale = 0.20
+        self.slit = 1.0
+        self.resolv = 2.0
+        
+        xthruput = np.zeros(self.npix1+self.npix2+self.npix3+self.npix4)
+        for i in range(0,self.npix1):
+            if channel == 'A': xthruput[i] = 0.117
+            if channel == 'B': xthruput[i] = 0.117
+            if channel == 'C': xthruput[i] = 0.138
+        for i in range(0,self.npix2):
+            if channel == 'A': xthruput[self.npix1+i] = 0.114
+            if channel == 'B': xthruput[self.npix1+i] = 0.130
+            if channel == 'C': xthruput[self.npix1+i] = 0.134
+        for i in range(0,self.npix3):
+            if channel == 'A': xthruput[self.npix1+self.npix2+i] = 0.115
+            if channel == 'B': xthruput[self.npix1+self.npix2+i] = 0.108
+            if channel == 'C': xthruput[self.npix1+self.npix2+i] = 0.114
+        for i in range(0,self.npix4):
+            if channel == 'A': xthruput[self.npix1+self.npix2+self.npix3+i] = 0.028
+            if channel == 'B': xthruput[self.npix1+self.npix2+self.npix3+i] = 0.020
+            if channel == 'C': xthruput[self.npix1+self.npix2+self.npix3+i] = 0.011
+
+        self.calwave = np.zeros(self.npix1+self.npix2+self.npix3+self.npix4)
+        calwave12 = np.concatenate((calwave1,calwave2),axis=0)
+        calwave123 = np.concatenate((calwave12,calwave3),axis=0)
+        self.calwave = np.concatenate((calwave123,calwave4),axis=0)
+
+        return xthruput
